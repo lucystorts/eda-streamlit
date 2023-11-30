13a00c3f190f1a7e7c1891f233f6d83aec37d60d
Lucy Storts <lucystorts@gmail.com>
2023-11-29 18:15:55 -0700

    edited py file

---
+++ edaLab10.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def run():
+    st.set_page_config(
+        page_title="Lab10",
+    )
+
+    st.write("# Lab 10")
+
+
+    st.markdown(
+        """
+        Text 
+    """
+    )
+
+
+if __name__ == "__main__":
+    run()
