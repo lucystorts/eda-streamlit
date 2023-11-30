33d378eaef3b0ba6a0c3557137aa7edc7429a88a
Lucy Storts <55722028+lucystorts@users.noreply.github.com>
2023-11-30 01:36:16 +0000

    adding to py file

--- edaLab10.py
+++ edaLab10.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+airports = pd.read_csv('2022Airports.csv')
+
 
 def run():
     st.set_page_config(
@@ -11,10 +17,15 @@ def run():
 
     st.markdown(
         """
-        Text 
+        ### EDA for Final Project
+
+        In this EDA, I want to exlpore the relationship bewteen *2022 Delays* proportion and other quantitative variables using a scatterplot.
+        
+        Then, I want to perform a regression analysis for these scatterplots.  
     """
     )
 
 
 if __name__ == "__main__":
     run()
+
